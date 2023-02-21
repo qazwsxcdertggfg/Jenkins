97a761edd0d1a1bb07a482bd9777bf73228cc002
Sahil <sahilnishi227@gmail.com>
2023-02-21 19:33:07 +0530

    Commit16

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "remote" {
-    hostname = "app.terraform.io"
+    #hostname = "app.terraform.io"
     organization = "Sahil123"
     workspaces {
       name = "gh-actions-demo"
