9f4fa1a0375e8aedf8aec4248eb0441373d55d1b
qazwsxcdertggfg <125578964+qazwsxcdertggfg@users.noreply.github.com>
2023-02-23 13:54:27 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -18,9 +18,9 @@ terraform {
 }
 
 provider "aws" {
-  #profile = "default"
-  AWS_ACCESS_KEY_ID="AKIATNOY6IAKLVWDFK76"
-  AWS_SECRET_ACCESS_KEY="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"
+  profile = "default"
+  #AWS_ACCESS_KEY_ID="AKIATNOY6IAKLVWDFK76"
+  #AWS_SECRET_ACCESS_KEY="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"
   region = "ap-south-1"
 }
 
