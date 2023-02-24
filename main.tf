a2636201ba2ef73878e062643c4fd98567122f8f
qazwsxcdertggfg <125578964+qazwsxcdertggfg@users.noreply.github.com>
2023-02-24 21:38:39 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,10 +2,10 @@ terraform {
   backend "remote" {
     hostname = "app.terraform.io"
     organization = "Sahil123"
-    token = "5ZyyIJyWOVVn1w.atlasv1.GvyeuMQzz9EG5hZR4fyuXOCixd1MhS47FMupwSCPwJTEfHEiARyGONl8zzIaNnzKTZE"
     workspaces {
       name = "sahil"
     }
+    token = "5ZyyIJyWOVVn1w.atlasv1.GvyeuMQzz9EG5hZR4fyuXOCixd1MhS47FMupwSCPwJTEfHEiARyGONl8zzIaNnzKTZE"
   }
 }
   
