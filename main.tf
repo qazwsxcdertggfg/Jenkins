878b57a7dbf9a6d93cf05edbdd8d28c7a0fc3187
qazwsxcdertggfg <125578964+qazwsxcdertggfg@users.noreply.github.com>
2023-02-24 20:44:47 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -22,7 +22,7 @@ provider "aws" {
   #profile = "default"
   #access_key="AKIATNOY6IAKLTKGUCLL"
   #secret_key="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"
-  region = "ap-south-1"
+  #region = "ap-south-1"
 }
 
 resource "aws_instance" "vm-web" {
